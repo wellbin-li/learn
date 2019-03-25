cac75654d874e4f198291a6e0e0a5003ecc28791
15629168655 <1084514417@qq.com>
2019-03-25 11:39:27 +0800

    二分查找

--- src/main/java/com/lwb/learn/datastructure/search/BinarySearch.java
+++ src/main/java/com/lwb/learn/datastructure/search/BinarySearch.java
@@ -11,10 +11,9 @@ public class BinarySearch {
         int low = 0;
         int high = a.length - 1;
         while (low <= high) {
-            int mid = (low + high) >>> 2;
-            if (target == a[mid]) {
-                return mid;
-            } else if (target < a[mid]) {
+            int mid = (low + high) >>> 1;
+            if (target == a[mid]) return mid;
+            if (target < a[mid]) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
